dc323c4f509f850011f38cd2560a45de2bed32ca
atif-khan <itsatifkhan871@gmail.com>
2024-11-13 15:30:18 +0500

    update and delete method added

--- grapheneapp/schema.py
+++ grapheneapp/schema.py
@@ -3,15 +3,25 @@ from graphene_django.types import DjangoObjectType
 from .models import Book
 import graphql_jwt
 
+
 class BookType(DjangoObjectType):
     class Meta:
         model = Book
 
+
 class Query(graphene.ObjectType):
     all_books = graphene.List(BookType)
+    book = graphene.Field(BookType, id=graphene.Int())
 
     def resolve_all_books(root, info):
         return Book.objects.all()
+
+    def resolve_book(root, info, id):
+        try:
+            return Book.objects.get(pk=id)
+        except Book.DoesNotExist:
+            return None
+
 
 class CreateBook(graphene.Mutation):
     class Arguments:
@@ -26,10 +36,55 @@ class CreateBook(graphene.Mutation):
         book.save()
         return CreateBook(book=book)
 
+
+class UpdateBook(graphene.Mutation):
+    class Arguments:
+        id = graphene.Int(required=True)
+        title = graphene.String()
+        author = graphene.String()
+        published_date = graphene.types.datetime.Date()
+
+    book = graphene.Field(BookType)
+
+    def mutate(self, info, id, title=None, author=None, published_date=None):
+        try:
+            book = Book.objects.get(pk=id)
+        except Book.DoesNotExist:
+            return None
+
+        if title is not None:
+            book.title = title
+        if author is not None:
+            book.author = author
+        if published_date is not None:
+            book.published_date = published_date
+
+        book.save()
+        return UpdateBook(book=book)
+
+
+class DeleteBook(graphene.Mutation):
+    class Arguments:
+        id = graphene.Int(required=True)
+
+    success = graphene.Boolean()
+
+    def mutate(self, info, id):
+        try:
+            book = Book.objects.get(pk=id)
+            book.delete()
+            return DeleteBook(success=True)
+        except Book.DoesNotExist:
+            return DeleteBook(success=False)
+
+
 class Mutation(graphene.ObjectType):
     create_book = CreateBook.Field()
+    update_book = UpdateBook.Field()
+    delete_book = DeleteBook.Field()
     token_auth = graphql_jwt.ObtainJSONWebToken.Field()
     verify_token = graphql_jwt.Verify.Field()
     refresh_token = graphql_jwt.Refresh.Field()
+
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
